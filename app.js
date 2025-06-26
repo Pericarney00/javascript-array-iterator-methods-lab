51e2fdc8f3a34f60e15f9b0b55aa79601f469edd
Peri Carney <pericarney@gmail.com>
2025-06-26 15:28:49 -0500

    completed exercise 3 and corrected numbet 2. I initially completed exercise 2 using .filter instead of .map

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ const comments = [
 
 // Lab Exercises
 /*
-Exercise 1: rray.prototype.filter()
+Exercise 1: Array.prototype.filter()
 
 Filter the array of inventors into a new array containing only the inventors 
 born in the 1500's.
@@ -135,9 +135,17 @@ Hint: Return a new object literal from the callback that looks like:
 let inventorNames = [];
 
 // Complete the exercise in the space below:
-inventorNames = inventors.filter((inventor) => {
-    return inventors.first + inventors.last
-})
+
+const namesArray = (inventor) => {
+    return {
+        first: inventor.first,
+        Last:  inventor.last,
+}}
+inventorNames = inventors.map(namesArray)
+
+// inventorNames = inventors.filter((inventor) => {
+//     return inventor.first + inventor.last
+// })
 // Check your work:
 console.log('Exercise 2 my result: ', inventorNames);
 console.log('Exercise 2 correct result: ', [
@@ -153,4 +161,41 @@ console.log('Exercise 2 correct result: ', [
   { first: 'Sarah E.', last: 'Goode' },
   { first: 'Lise', last: 'Meitner' },
   { first: 'Hanna', last: 'Hammarström' },
+]);
+
+/*
+Exercise 3: Array.prototype.sort()
+
+Sort the inventors by birth date in ascending order (from those born furthest in
+the past to those born most recently).
+*/
+
+let sortedByBirthYear = [];
+
+
+const oldestInventor = inventors[5].year
+const youngestInventor = inventors[7].year
+console.log(oldestInventor)
+
+// Complete the exercise in the space below:
+
+sortedByBirthYear = inventors.sort((a, b)  => {
+    return  a.year- b.year
+}) 
+
+// Check your work:
+console.log('Exercise 3 my result: ', sortedByBirthYear);
+console.log('Exercise 3 correct result: ', [
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+  { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 },
+  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+  { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
 ]);
